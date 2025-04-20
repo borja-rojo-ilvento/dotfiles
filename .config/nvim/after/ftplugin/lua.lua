a202767477c4660b6357f4ea603a33d49e5779d4
Borja Rojo Ilvento <borja.rojo@gmail.com>
2025-04-20 14:49:28 -0400

    Add the submodule

--- .config/nvim/after/ftplugin/lua.lua
+++
@@ -1,2 +0,0 @@
-
-vim.bo.shiftwidth = 2
--- .config/nvim/lua/plugins/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"echasnovski/mini.nvim",
-		version = "*",
-		config = function()
-			require("mini.statusline").setup({ useIcons = true })
-		end
-	}
-}
