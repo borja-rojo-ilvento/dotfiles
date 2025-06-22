345277a9921b94cc23afa023519119f6d964d370
Borja Rojo Ilvento <borja.rojo@gmail.com>
2025-06-22 18:43:20 -0400

    Adding home.nix to import to my user.

---
+++ home.nix
@@ -0,0 +1,23 @@
+{ pkgs, lib, ... }:
+let
+  dotfiles = pkgs.fetchFromGitHub {
+    owner = "yourusername";
+    repo = "dotfiles";
+    rev = "main";
+    sha256 = "...";
+  };
+in
+{
+  home.packages = with pkgs; [ stow ];
+
+  home.file."dotfiles" = {
+    source = dotfiles;
+    recursive = true;
+  };
+
+  home.activation.stowDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    pushd $HOME/dotfiles
+    ${pkgs.stow}/bin/stow .
+    popd
+  '';
+}
