cbf85b7022bf024811e4ad4f31cd1b8ee8deff97
Borja Rojo Ilvento <borja.rojo@gmail.com>
2025-06-22 22:01:25 -0400

    Formatting.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
-  description = "A very basic flake";
+  description = "Expose a home-manager module with injects these dotfiles into the dotfiles/ directory, and runs `stow .` to copy them in.";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
+  inputs = { };
   outputs =
-    { self, nixpkgs }:
+    { self }:
     {
       nixosModules.default = import ./home.nix { dotfiles = self; };
-
     };
 }
